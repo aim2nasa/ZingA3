7697499634256925e56ddc197a7ec85a75518831
Rossi Kwak <rossi.kwak@gmail.com>
2020-11-11 16:18:47 +0900

    Send received buffer from GPIF to channel glDataOutCh which represents data out from the producer of CPU(fx3) to the consumer of USB(SmartPhone)

--- src/dma.c
+++ src/dma.c
@@ -3,6 +3,8 @@
 #include "cyu3usb.h"
 #include "cyu3system.h"
 #include "macro.h"
+#include "Zing.h"
+#include "phonedrv.h"
 
 static void initDmaCount()
 {
@@ -102,7 +104,12 @@ void DMA_Sync_DataIn_Cb(CyU3PDmaChannel *handle,CyU3PDmaCbType_t evtype,CyU3PDma
 {
 	switch (evtype)
 	{
+	CyU3PReturnStatus_t status;
 	case CY_U3P_DMA_CB_PROD_EVENT:
+    	if((status=Zing_Transfer_Send(&glDataOutCh,input->buffer_p.buffer,input->buffer_p.count))!=CY_U3P_SUCCESS) {
+    		CyU3PDebugPrint (1, "[DMA_Sync_DataIn_Cb] Zing_Transfer_Send error=0x%x\n",status);
+    	}
+
 		CyU3PDebugPrint (6, "DMA_Sync_DataIn_Cb->CY_U3P_DMA_CB_PROD_EVENT, buffer(count:%d,size:%d,status:%d)\n",
 				input->buffer_p.count,input->buffer_p.size,input->buffer_p.status);
 
